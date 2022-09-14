dad488feba4fb07117cec7285d8b421983c752a3
patricklouback <patrickmlouback@gmail.com>
2022-09-14 12:39:00 -0300

    evoluindo no projeto admin-template curso nextjs

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -8,14 +8,18 @@ interface LayoutProps {
     children?: any
 }
 
-export default function Layout (props: LayoutProps) {
+export default function Layout(props: LayoutProps) {
     return (
-        <div>
-            <MenuLateral/>
-            <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo}/>
-            <Conteudo>
-                {props.children}
-            </Conteudo>
+        <div className={`
+        dark flex h-screen w-screen
+        `}>
+                <MenuLateral />
+            <div className={`flex flex-col w-full p-7 bg-gray-300 dark:bg-gray-800`}>
+                <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
+                <Conteudo>
+                    {props.children}
+                </Conteudo>
+            </div>
         </div>
     )
 }
---
+++ src/pages/ajustes.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/template/Layout";
+
+export default function Ajustes() {
+  return (
+    <Layout titulo="Ajustes" subtitulo="Gerenciar notificações">
+      <h3>Conteudo</h3>
+    </Layout>
+  )
+}
